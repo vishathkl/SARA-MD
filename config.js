91cc86da25935f352145d7fefab7f319f98d6773
vishathkl <saramdbot456@gmail.com>
2025-10-24 19:20:15 +0530

    Update bot owner and alive message in config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello🌹💛 SARA-MD Is Alive Now🧡💚💙💛❤️*",
+BOT_OWNER: '94765346615',  // Replace with the owner's phone number
 
 
 
